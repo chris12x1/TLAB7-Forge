9a7a2eab4e2978651c29b7f92a204c67b5cf3c37
Christopher Diaz <christopher.diaz87@yahoo.com>
2026-08-04 19:44:32 -0400

    Secured ingress rules

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ resource "aws_security_group" "sabotaged_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SABOTAGE: SSH exposed to the world
+    cidr_blocks = ["1.2.3.4/32"] # SABOTAGE: SSH exposed to the world
   }
 }
